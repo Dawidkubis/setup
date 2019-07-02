c39e9dcb23b057638a04fd9ef0a95628112557e8
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-07-02 22:40:27 +0200

    .

--- install.py
+++ install.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python
 import os
 import sys
-import termcolor
+
+class Rror(Exception):
+    def __init__(*args,**kwargs):
+        super().__init__(*args, **kwargs)
+        self.args = args
+        self.kwargs = kwargs
+
+# Check if have root priviledges
+assert os.environ['USER'] == 'root', 'This script must be run as root!'
+
+# Check if on linux
+assert sys.platform in ('linux','linux2'), 'This script must be run on linux!'
+
+# Ask about platform
+
+
+# Install packages
+
+
+# Install aur
+
+
+# Install configs
+
+
+# Move configs
+
+
+# Install APPS (sync and self-info)
+
+
