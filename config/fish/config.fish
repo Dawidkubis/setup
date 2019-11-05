0f79a071d9c0a89bca7c838ecc31a4062b6b209c
Dawid J. Kubis <dawid.kubis3@gmail.com>
2019-11-05 14:39:05 +0100

    fuck it i like alacritty better

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,5 +5,3 @@ set -xU EDITOR nvim
 alias ll "exa -l"
 alias la "exa -la"
 alias diff "diff --color=always"
-alias icat "kitty +kitten icat"
-alias neofetch "neofetch --kitty ~/.config/neofetch/kliszmoszek.png"
